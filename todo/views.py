f25c07a9d0f7e00d83da35ace0b3b9a74e858e67
Alen Jojo <alenjojo252001@gmail.com>
2022-10-28 17:33:43 +0530

    added existing user ex

--- todo/views.py
+++ todo/views.py
@@ -16,11 +16,14 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-
+            try:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'That username has already been taken    '})
 
         else:
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Invalid Credentials'})
